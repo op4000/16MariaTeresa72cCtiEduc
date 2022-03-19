aceee0bdbcff013f2ac18a557ee9f231ef5c8031
op4000 <matheusgames123429@gmail.com>
2022-03-19 15:41:45 -0300

    Adicionar arquivos de projeto.

---
+++ 16MariaTeresa72cCtiEduc/frmFrase.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace _16MariaTeresa72cCtiEduc
+{
+    partial class frmFrase
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBloqueado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBloqueado = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtEvidencias = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAqueda = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnQueda = new System.Windows.Forms.Button();
+            this.btnEvidencias = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnRefazer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(-21, -3);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(524, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Organize a letra das músicas a seguir:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(424, 52);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "é tristeza raiva que muita misturada Lembrei com bloqueado to";
+            // 
+            // txtBloqueado
+            // 
+            this.txtBloqueado.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBloqueado.Location = new System.Drawing.Point(11, 143);
+            this.txtBloqueado.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBloqueado.Name = "txtBloqueado";
+            this.txtBloqueado.Size = new System.Drawing.Size(449, 20);
+            this.txtBloqueado.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.label3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 228);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(448, 62);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "vou te quero que não nessa negando as loucura evidências E dizer disfarçando de a" +
+    "s  aparências";
+            // 
+            // btnBloqueado
+            // 
+            this.btnBloqueado.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBloqueado.Location = new System.Drawing.Point(425, 165);
+            this.btnBloqueado.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBloqueado.Name = "btnBloqueado";
+            this.btnBloqueado.Size = new System.Drawing.Size(36, 23);
+            this.btnBloqueado.TabIndex = 4;
+            this.btnBloqueado.Text = "ok";
+            this.btnBloqueado.UseVisualStyleBackColor = true;
+            this.btnBloqueado.Click += new System.EventHandler(this.btnBloqueado_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(406, 515);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(70, 33);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtEvidencias
+            // 
+            this.txtEvidencias.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEvidencias.Location = new System.Drawing.Point(6, 292);
+            this.txtEvidencias.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEvidencias.Name = "txtEvidencias";
+            this.txtEvidencias.Size = new System.Drawing.Size(449, 20);
+            this.txtEvidencias.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.label4.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 356);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(449, 67);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "vai  um  tão show vai Respeitável público noite maluco gente essa circo armar aqu" +
+    "i um acontecer a";
+            // 
+            // txtAqueda
+            // 
+            this.txtAqueda.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAqueda.Location = new System.Drawing.Point(11, 425);
+            this.txtAqueda.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAqueda.Name = "txtAqueda";
+            this.txtAqueda.Size = new System.Drawing.Size(449, 20);
+            this.txtAqueda.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(305, 126);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 14);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "DICA: Música do Gustavo Lima";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(265, 276);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(190, 14);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "DICA: Música do Chitãozinho e Xororó";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(306, 409);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(156, 14);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "DICA: Música da Gloria Groove";
+            // 
+            // btnQueda
+            // 
+            this.btnQueda.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQueda.Location = new System.Drawing.Point(424, 449);
+            this.btnQueda.Margin = new System.Windows.Forms.Padding(2);
+            this.btnQueda.Name = "btnQueda";
+            this.btnQueda.Size = new System.Drawing.Size(36, 23);
+            this.btnQueda.TabIndex = 13;
+            this.btnQueda.Text = "ok";
+            this.btnQueda.UseVisualStyleBackColor = true;
+            this.btnQueda.Click += new System.EventHandler(this.btnQueda_Click);
+            // 
+            // btnEvidencias
+            // 
+            this.btnEvidencias.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEvidencias.Location = new System.Drawing.Point(426, 317);
+            this.btnEvidencias.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEvidencias.Name = "btnEvidencias";
+            this.btnEvidencias.Size = new System.Drawing.Size(36, 23);
+            this.btnEvidencias.TabIndex = 14;
+            this.btnEvidencias.Text = "ok";
+            this.btnEvidencias.UseVisualStyleBackColor = true;
+            this.btnEvidencias.Click += new System.EventHandler(this.btnEvidencias_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(161, 165);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(260, 15);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "OBS: nenhuma letra maiúscula e nem acento ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(162, 314);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(260, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "OBS: nenhuma letra maiúscula e nem acento ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(160, 449);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(260, 15);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "OBS: nenhuma letra maiúscula e nem acento ";
+            // 
+            // btnRefazer
+            // 
+            this.btnRefazer.Location = new System.Drawing.Point(268, 515);
+            this.btnRefazer.Name = "btnRefazer";
+            this.btnRefazer.Size = new System.Drawing.Size(85, 33);
+            this.btnRefazer.TabIndex = 19;
+            this.btnRefazer.Text = "Refazer";
+            this.btnRefazer.UseVisualStyleBackColor = true;
+            this.btnRefazer.Click += new System.EventHandler(this.btnRefazer_Click_1);
+            // 
+            // frmFrase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(484, 558);
+            this.Controls.Add(this.btnRefazer);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnEvidencias);
+            this.Controls.Add(this.btnQueda);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAqueda);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEvidencias);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnBloqueado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBloqueado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmFrase";
+            this.Text = "Organize as Músicas (mínimo 13 anos)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmFrase_FormClosing);
+            this.Load += new System.EventHandler(this.frmFrase_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBloqueado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBloqueado;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtEvidencias;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAqueda;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnQueda;
+        private System.Windows.Forms.Button btnEvidencias;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnRefazer;
+    }
+}
+
---
+++ 16MariaTeresa72cCtiEduc/frmFrase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16MariaTeresa72cCtiEduc
+{
+    public partial class frmFrase : Form
+    {
+        public frmFrase()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void frmFrase_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult resposta;
+            resposta = MessageBox.Show("Você realmente deseja sair?",
+                            "Cadastro de Curso",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question);
+
+            if (resposta == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+
+        }
+
+        private void frmFrase_Load(object sender, EventArgs e)
+        {
+            btnRefazer.Visible = false;
+        }
+
+        private void btnBloqueado_Click(object sender, EventArgs e)
+        {
+            if (txtBloqueado.Text == "lembrei que to bloqueado e muita raiva misturada com tristeza") 
+            {
+                MessageBox.Show("Você acertou", "Acertou", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                txtBloqueado.Enabled = false;
+                txtBloqueado.BackColor = Color.Green;
+            }
+            else
+            {
+                MessageBox.Show("Você errou", "Errou", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+
+        private void btnEvidencias_Click(object sender, EventArgs e)
+        {
+            if (txtEvidencias.Text == "e nessa loucura de dizer que nao te quero vou negando as aparencias disfarçando as evidencias")
+            {
+                MessageBox.Show("Você acertou", "Acertou", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                txtEvidencias.Enabled = false;
+                txtEvidencias.BackColor = Color.Green;
+                txtEvidencias.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Você errou", "Errou", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+
+        private void btnQueda_Click(object sender, EventArgs e)
+        {
+                if (txtAqueda.Text == "respeitavel publico um show tao maluco essa noite vai acontecer aqui a gente vai armar um circo")
+                {
+                    MessageBox.Show("Você acertou", "Acertou", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                    txtAqueda.Enabled = false;
+                    txtAqueda.BackColor = Color.Green;
+                    btnRefazer.Visible = true;
+                    txtAqueda.Text = "";
+            }
+                else
+                {
+                    MessageBox.Show("Você errou", "Errou", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                }
+        }
+
+        private void btnRefazer_Click_1(object sender, EventArgs e)
+        {
+            txtBloqueado.Enabled = true;
+            txtEvidencias.Enabled = true;
+            txtAqueda.Enabled = true;
+            txtBloqueado.BackColor = Color.White;
+            txtEvidencias.BackColor = Color.White;
+            txtAqueda.BackColor = Color.White;
+            btnRefazer.Visible = false;
+            txtEvidencias.Text = "";
+            txtAqueda.Text = "";
+            txtBloqueado.Text = "";
+        }
+    }
+}
